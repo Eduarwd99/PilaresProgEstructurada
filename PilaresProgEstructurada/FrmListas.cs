581d7c3323ac7457e5ad28a7bdcbc8e7e8555e51
G <G@PC>
2020-06-01 10:59:23 -0500

    Eventos Click y Boton

---
+++ PilaresProgEstructurada/FrmListas.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PilaresProgEstructurada
+{
+    public partial class FrmListas : Form
+    {
+        public FrmListas()
+        {
+            InitializeComponent();
+        }
+
+        private void lstNombres_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if(this.txtNombre.Text=="")
+            {
+
+            }
+            else
+            {
+                this.lstNombres.Items.Add(this.txtNombre.Text);
+                this.txtNombre.Text = "";
+            }
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtNombre_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.Equals(Keys.Enter))
+            {
+                if (this.txtNombre.Text == "")
+                {
+
+                }
+                else
+                {
+                    this.lstNombres.Items.Add(this.txtNombre.Text);
+                    this.txtNombre.Text = "";
+                }
+            }
+        }
+    }
+}
